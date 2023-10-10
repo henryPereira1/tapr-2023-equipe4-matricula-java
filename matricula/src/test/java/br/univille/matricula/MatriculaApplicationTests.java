52c968e7dfcbf314d7e501ec6e744bca8ffd84dd
henryPereira1 <128191946+henryPereira1@users.noreply.github.com>
2023-10-10 00:39:37 +0000

    ajustes

---
+++ matricula/src/test/java/br/univille/matricula/MatriculaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.matricula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
